b35b337ed4962098667177944a59c01a5bdffef8
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2024-09-18 20:03:37 -0400

    changer le port

--- index.py
+++ index.py
@@ -36,10 +36,11 @@ def index():
 def yes():
     return render_template('yes.html')
 
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html'), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(port=5001, debug=True))
