2ab7e92ab0449f1b71b285dfc10e1a27f386c0ef
Juan Carlos Merida Cortes <ah291956@ens.uqam.ca>
2025-12-22 21:48:05 -0500

    Added /no page with a little secret :)

--- static/script.js
+++ static/script.js
@@ -2,17 +2,20 @@ const yesButton = document.querySelector('#yes-button');
 const noButton = document.querySelector('#no-button');
 
 const moveNoButton = () => {
-  var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
-  var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+    var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+    var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
 
-  noButton.style.position = 'absolute';
-  noButton.style.left = `${x}px`;
-  noButton.style.top = `${y}px`;
+    noButton.style.position = 'absolute';
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
 }
 
 yesButton.addEventListener('click', () => {
-  window.location.href = '/yes';
+    window.location.href = '/yes';
 })
 
-noButton.addEventListener('click', moveNoButton);
 noButton.addEventListener('mouseenter', moveNoButton);
+noButton.addEventListener('click', () => {
+    window.location.href = '/no';
+})
+
